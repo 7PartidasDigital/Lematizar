56af2d6ac7785c693cee70586e998ff3784b9d03
7 Partidas Digital <7partidasdigital@gmail.com>
2025-03-27 12:49:30 +0100

    Primera carga

---
+++ stm_topics.R
@@ -0,0 +1,50 @@
+####################################################
+# Script para replicar el análisis de tópicos con  #
+# stm que se discute en el el artículo "Lematizar  #
+# o no lematizar, esa es la cuestión: las Siete    #
+# Partidas desde el Topic Modeling" de J. M.       #
+# Fradejas Rueda publicado en "Claro espejo donde  #
+# la ilusión se mira. Homenaje a Pilar Celma Valero#
+# y Javier Blasco Pacual. Valladolid, Universidad  #
+# Valladolid, 2025.                                #
+####################################################
+
+library(tidyverse)
+library(tidytext)
+library(stm)
+partidas_tabla <- read_tsv("IOC-POS-tabla.txt")
+
+partidas_sparse <- partidas_tabla %>%
+  drop_na() %>%
+  #filter(upos == "NOUN" | upos == "ADJ" | upos == "VERB") %>% # Esta línea se activa (borrar el #) solo selecciona SUST., ADJ., VERB.
+  filter(str_detect(lema, "[^0-9]+")) %>% # Borra los lemas cuyo valor sea número. Para explorar por palabras se anula línea con # antes de filter
+  filter(upos != "PROPN") %>% # Borra los nombres propios Si se quiere desactivar, hay que poner un # antes de filter
+  count(ley, lema) %>% # Cambiar ley | titulo y lema | palabra según se desee por leyes o títulos y por lema o palabras
+  cast_sparse(ley, lema, n) # Cambiar ley | titulo y lema | palabra según se desee por leyes o títulos y por lemas o palabras
+dim(partidas_sparse)
+
+set.seed(1234)
+topic_model <- stm(partidas_sparse, K = 16, verbose = FALSE) # el valor de K es el que deterimina nº tópicos
+
+# Muestra en la conssola las n primera palabras de cada tópico
+labelTopics(topic_model, n = 10)
+
+partidas_gamma <- tidy(
+  topic_model, 
+  matrix = "gamma",
+  document_names = rownames(partidas_sparse)
+) 
+
+# Imprimer los gráficos de bigotes con
+partidas_gamma %>% 
+  left_join(
+    partidas_tabla %>% 
+      select(partida, document = ley) %>% # Aquí hay que usar titulo | ley depende de lo que se haya seleccionado antes
+      mutate(partida = fct_inorder(partida))
+  ) %>%
+  mutate(topic = factor(topic)) %>% 
+  ggplot(aes(gamma, topic, fill = topic)) +
+  geom_boxplot(alpha = 0.7, show.legend = FALSE) +
+  facet_wrap(vars(partida), ncol = 4) +
+  labs(x = expression(gamma))
+
